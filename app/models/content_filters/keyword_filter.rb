a1c1f20fa23c739ed89e202b5ff7ce49830387b6
Min Khant Kyaw <minkhantkyawmdy35@gmail.com>
2024-10-08 13:46:25 +0630

    Update keyword_filter.rb

--- app/models/content_filters/keyword_filter.rb
+++ app/models/content_filters/keyword_filter.rb
@@ -2,7 +2,7 @@ module ContentFilters
   class KeywordFilter < ApplicationRecord
     self.table_name = 'keyword_filters'
 
-    enum filter_type: { hashtag: 0, both: 1, content: 2 }
+    enum filter_type:{ content: 0, hashtag: 1, both: 2 }
 
   end
 end
