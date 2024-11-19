0d0771ae293caff58791ff4ac6a09326f2eece0f
Min Khant Kyaw <minkhantkyawmdy35@gmail.com>
2024-11-19 18:07:55 +0630

    Update ban status worker

--- app/workers/ban_status_worker.rb
+++ app/workers/ban_status_worker.rb
@@ -4,10 +4,12 @@ class BanStatusWorker
   include Sidekiq::Worker
 
   def perform(status_id)
+    status = Status.find_by(id: status_id)
+
     is_status_banned = ContentFilters::BanStatusService.new.check_and_ban_status(status_id)
     
     # Channel admin reblog related sub-channles service
-    ReblogChannelsService.new.call(@status) if ENV.fetch('MAIN_CHANNEL', nil) != 'false' && ENV.fetch('MAIN_CHANNEL', nil) != nil && !is_status_banned
+    ReblogChannelsService.new.call(status) if ENV.fetch('MAIN_CHANNEL', nil) != 'false' && ENV.fetch('MAIN_CHANNEL', nil) != nil && !is_status_banned
 
   end
 end
