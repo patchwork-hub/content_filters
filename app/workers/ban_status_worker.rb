2a2eafc2a980c4856496899f6b02dc0ae781d718
Aung Kyaw Phyo <kiru.kiru28@gmail.com>
2025-02-13 20:46:24 +0630

    Enable filters

--- app/workers/ban_status_worker.rb
+++ app/workers/ban_status_worker.rb
@@ -6,10 +6,10 @@ class BanStatusWorker
   def perform(status_id)
     status = Status.find_by(id: status_id)
 
-    # is_status_banned = ContentFilters::BanStatusService.new.check_and_ban_status(status_id)
+    is_status_banned = ContentFilters::BanStatusService.new.check_and_ban_status(status_id)
     
     # Channel admin reblog related sub-channles service
-    ReblogChannelsService.new.call(status) if ENV.fetch('MAIN_CHANNEL', nil) != 'false' && ENV.fetch('MAIN_CHANNEL', nil) != nil # && !is_status_banned
+    ReblogChannelsService.new.call(status) if ENV.fetch('MAIN_CHANNEL', nil) != 'false' && ENV.fetch('MAIN_CHANNEL', nil) != nil && !is_status_banned
 
   end
 end
