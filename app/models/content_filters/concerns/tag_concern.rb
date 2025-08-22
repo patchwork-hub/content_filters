56da011d83fb9e474101defb87f64312543e44e7
Aung Kyaw Phyo <kiru.kiru28@gmail.com>
2025-08-22 12:19:58 +0700

    Add ban case consideration in the tag fetching

--- app/models/content_filters/concerns/tag_concern.rb
+++ app/models/content_filters/concerns/tag_concern.rb
@@ -7,17 +7,31 @@ module ContentFilters::Concerns::TagConcern
       scope :without_banned, -> { where(is_banned: false) }
     end
 
-    # # Override class methods
-    # base.singleton_class.class_eval do
-    #   def matching_name(name_or_names)
-    #     names = Array(name_or_names).map { |name| arel_table.lower(normalize(name)) }
+    # Override class methods
+    base.singleton_class.class_eval do
+      def matching_name(name_or_names, consider_ban_case=true)
+        names = Array(name_or_names).map { |name| arel_table.lower(normalize(name)) }
 
-    #     if names.size == 1
-    #       where(arel_table[:name].lower.eq(names.first)).where(is_banned: false)
-    #     else
-    #       where(arel_table[:name].lower.in(names)).where(is_banned: false)
-    #     end
-    #   end
-    # end
+        scope = if names.size == 1
+          where(arel_table[:name].lower.eq(names.first))
+        else
+          where(arel_table[:name].lower.in(names))
+        end
+        
+        scope = scope.where(is_banned: false) if consider_ban_case
+        scope
+      end
+
+      def find_or_create_by_names(name_or_names)
+        names = Array(name_or_names).map { |str| [normalize(str), str] }.uniq(&:first)
+
+        names.map do |(normalized_name, display_name)|
+          tag = matching_name(normalized_name, false).first || create(name: normalized_name,
+                                                              display_name: display_name.gsub(HASHTAG_INVALID_CHARS_RE, ''))
+          yield tag if block_given?
+          tag
+        end
+      end
+    end
   end
 end
