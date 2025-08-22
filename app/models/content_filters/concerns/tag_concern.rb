81ba7fe9433770c4ec19cc8a6d7ee094736c7b88
Aung Kyaw Phyo <kiru.kiru28@gmail.com>
2025-08-22 11:57:16 +0700

    Comment out issue causing code block

--- app/models/content_filters/concerns/tag_concern.rb
+++ app/models/content_filters/concerns/tag_concern.rb
@@ -7,17 +7,17 @@ module ContentFilters::Concerns::TagConcern
       scope :without_banned, -> { where(is_banned: false) }
     end
 
-    # Override class methods
-    base.singleton_class.class_eval do
-      def matching_name(name_or_names)
-        names = Array(name_or_names).map { |name| arel_table.lower(normalize(name)) }
+    # # Override class methods
+    # base.singleton_class.class_eval do
+    #   def matching_name(name_or_names)
+    #     names = Array(name_or_names).map { |name| arel_table.lower(normalize(name)) }
 
-        if names.size == 1
-          where(arel_table[:name].lower.eq(names.first)).where(is_banned: false)
-        else
-          where(arel_table[:name].lower.in(names)).where(is_banned: false)
-        end
-      end
-    end
+    #     if names.size == 1
+    #       where(arel_table[:name].lower.eq(names.first)).where(is_banned: false)
+    #     else
+    #       where(arel_table[:name].lower.in(names)).where(is_banned: false)
+    #     end
+    #   end
+    # end
   end
 end
