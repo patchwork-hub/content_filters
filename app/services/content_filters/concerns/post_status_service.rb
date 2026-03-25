7fc663fb42a9052eb913bbc9d7d234ee5c74b04a
Min Khant Kyaw <minkhantkyawmdy35@gmail.com>
2026-03-25 18:03:11 +0630

    update post status services

--- app/services/content_filters/concerns/post_status_service.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module ContentFilters::Concerns::PostStatusService
-  def postprocess_status!
-    super
-
-    BanStatusWorker.perform_async(@status.id, { 'from' => 'post_status_service' }) if @status&.id.present?
-  end
-end
