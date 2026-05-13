532cbbbbfa1c642d1d610f3e1e1c149c0b5bc897
Nyan Lin Htut <nyanlinhtut925@gmail.com>
2026-05-13 18:32:47 +0700

    rename notification_sent_at migration file

--- db/migrate/20260513100001_add_missing_notification_sent_at_to_announcements.rb
+++ db/migrate/20260513100001_add_missing_notification_sent_at_to_announcements.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddNotificationSentAtToAnnouncements < ActiveRecord::Migration[7.0]
+class AddMissingNotificationSentAtToAnnouncements < ActiveRecord::Migration[7.0]
   def change
     unless column_exists?(:announcements, :notification_sent_at)
       add_column :announcements, :notification_sent_at, :datetime
